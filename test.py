a48ee479fa574442538528934f97c0487f62bc16
Rajan Selvan <8602178+rjslvn@users.noreply.github.com>
2023-10-01 19:54:52 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,115 @@
+import numpy as np
+import tensorflow as tf
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, BertTokenizer, BertModel, ViTFeatureExtractor, ViTModel
+import torch
+
+# Hyperparameters
+num_epochs = 10
+batch_size = 32
+num_classes = 10
+num_frames = 100
+num_mfcc_features = 13
+num_text_features = 1000
+image_height = 128
+image_width = 128
+num_channels = 3
+
+# Input Shapes for different modalities
+voice_input_shape = (num_frames, num_mfcc_features)
+text_input_shape = (num_text_features,)
+sight_input_shape = (image_height, image_width, num_channels)
+
+# Custom Loss Function
+def custom_loss(y_true, y_pred):
+    voice_loss = tf.keras.losses.MeanSquaredError()(y_true, y_pred)
+    text_loss = tf.keras.losses.MeanSquaredError()(y_true, y_pred)
+    sight_loss = tf.keras.losses.MeanSquaredError()(y_true, y_pred)
+    fusion_loss = voice_loss + text_loss + sight_loss
+    final_loss = voice_loss + text_loss + sight_loss + 0.1 * fusion_loss
+    return final_loss
+
+# Model Architecture
+def build_multimodal_model(input_shape):
+    input_layer = tf.keras.layers.Input(shape=input_shape)
+    x = tf.keras.layers.Dense(128, activation='relu')(input_layer)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    return model
+
+# Action 2: Transform Voice Inputs
+def transform_voice(normalized_audio):
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+    input_values = processor(normalized_audio, return_tensors="pt").input_values
+    with torch.no_grad():
+        logits = model(input_values).logits
+    return logits
+
+# Action 3: Transform Text Inputs
+def transform_text(tokenized_text):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertModel.from_pretrained('bert-base-uncased')
+    inputs = tokenizer(tokenized_text, return_tensors="pt")
+    outputs = model(**inputs)
+    return outputs.last_hidden_state
+
+# Action 4: Transform Sight Inputs
+def transform_sight(loaded_image):
+    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
+    model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+    inputs = feature_extractor(loaded_image, return_tensors="pt")
+    outputs = model(**inputs)
+    return outputs.last_hidden_state
+
+# Action 5: Feature Engineering for Voice
+def feature_voice(logits):
+    return logits
+
+# Action 6: Feature Engineering for Text
+def feature_text(text_vector):
+    return text_vector
+
+# Action 7: Feature Engineering for Sight
+def feature_sight(sight_transformed):
+    return sight_transformed
+
+# Action 8: Implement Early Fusion
+def early_fusion(voice_features, text_features, sight_features):
+    return torch.cat((voice_features, text_features, sight_features), dim=-1)
+
+# Action 9: Implement Late Fusion
+def late_fusion(voice_output, text_output, sight_output):
+    return 0.3 * voice_output + 0.3 * text_output + 0.4 * sight_output
+
+# Action 10: Implement Hybrid Fusion
+def hybrid_fusion(early_fusion_output, late_fusion_output):
+    return 0.5 * early_fusion_output + 0.5 * late_fusion_output
+
+# Action 11: Design Model Architecture
+def design_model(early_fusion_output, late_fusion_output):
+    voice_input = tf.keras.layers.Input(shape=(early_fusion_output.shape[1],))
+    text_input = tf.keras.layers.Input(shape=(early_fusion_output.shape[1],))
+    sight_input = tf.keras.layers.Input(shape=(early_fusion_output.shape[1],))
+
+    x = tf.keras.layers.Concatenate()([voice_input, text_input, sight_input])
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
+    output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+    model = tf.keras.Model(inputs=[voice_input, text_input, sight_input], outputs=output)
+    return model
+
+# Execute Actions
+voice_logits = transform_voice(normalized_audio)
+text_vector = transform_text(tokenized_text)
+sight_vector = transform_sight(loaded_image)
+
+early_fusion_output = early_fusion(voice_logits, text_vector, sight_vector)
+late_fusion_output = late_fusion(voice_logits, text_vector, sight_vector)
+hybrid_fusion_output = hybrid_fusion(early_fusion_output, late_fusion_output)
+
+model = build_multimodal_model(early_fusion_output.shape[1])
+model.compile(optimizer='adam', loss=custom_loss)
+
+# Further code for training, evaluation, and deployment can be added here
