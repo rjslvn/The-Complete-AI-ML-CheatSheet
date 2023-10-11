b27a778cf3569b928df113fc7c89a96adb1c02b7
Raj <8602178+rjslvn@users.noreply.github.com>
2023-10-11 03:40:44 -0700

    Update BUILD_ONE.py

--- BUILD_ONE.py
+++ BUILD_ONE.py
@@ -1,11 +1,11 @@
-import numpy as np
-import tensorflow as tf
-from transformers import (Wav2Vec2ForCTC, Wav2Vec2Processor, BertTokenizer, 
-                          BertModel, ViTFeatureExtractor, ViTModel)
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
+import numpy as np
+import tensorflow as tf
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, BertTokenizer, BertModel, ViTFeatureExtractor, ViTModel
 
 # ========================= HYPERPARAMETERS =========================
 
@@ -22,7 +22,6 @@ NUM_CHANNELS = 3
 VOICE_INPUT_SHAPE = (NUM_FRAMES, NUM_MFCC_FEATURES)
 TEXT_INPUT_SHAPE = (NUM_TEXT_FEATURES,)
 SIGHT_INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS)
-
 
 # ========================= TRANSFORMERS =========================
 
@@ -48,153 +47,45 @@ def transform_sight(loaded_image):
     outputs = model(**inputs)
     return outputs.last_hidden_state
 
-
 # ========================= FUSION MECHANISMS =========================
 
 def early_fusion(voice_features, text_features, sight_features):
-    return torch.cat((voice_features, text_features, sight_features), dim=-1)
+    return tf.concat([voice_features, text_features, sight_features], axis=-1)
 
 def late_fusion(voice_output, text_output, sight_output):
     return 0.3 * voice_output + 0.3 * text_output + 0.4 * sight_output
-
-def hybrid_fusion(early_fusion_output, late_fusion_output):
-    return 0.5 * early_fusion_output + 0.5 * late_fusion_output
-
 
 # ========================= CUSTOM LOSS FUNCTION =========================
 
-def custom_loss(y_true_voice, y_true_text, y_true_sight, y_pred_voice, y_pred_text, y_pred_sight):
-    voice_loss = tf.keras.losses.MeanSquaredError()(y_true_voice, y_pred_voice)
-    text_loss = tf.keras.losses.MeanSquaredError()(y_true_text, y_pred_text)
-    sight_loss = tf.keras.losses.MeanSquaredError()(y_true_sight, y_pred_sight)
+def custom_loss(y_true, y_pred):
+    voice_loss = tf.keras.losses.MeanSquaredError()(y_true[0], y_pred[0])
+    text_loss = tf.keras.losses.MeanSquaredError()(y_true[1], y_pred[1])
+    sight_loss = tf.keras.losses.MeanSquaredError()(y_true[2], y_pred[2])
     fusion_loss = voice_loss + text_loss + sight_loss
-    final_loss = voice_loss + text_loss + sight_loss + 0.1 * fusion_loss
-    return final_loss
-
+    return fusion_loss + 0.1 * fusion_loss
 
 # ========================= MULTIMODAL MODEL =========================
 
-class MultimodalModel(tf.keras.Model):
-    def __init__(self, voice_model, text_model, sight_model):
-        super(MultimodalModel, self).__init__()
-        self.voice_model = voice_model
-        self.text_model = text_model
-        self.sight_model = sight_model
-
-    def call(self, inputs, training=False):
-        voice_input, text_input, sight_input = inputs
-        voice_features = self.voice_model(voice_input)
-        text_features = self.text_model(text_input)
-        sight_features = self.sight_model(sight_input)
-
-# Action 8: Implement Early Fusion
-def early_fusion(voice_features, text_features, sight_features):
-    return torch.cat((voice_features, text_features, sight_features), dim=-1)
-
-# Action 9: Implement Late Fusion
-def late_fusion(voice_output, text_output, sight_output):
-    return 0.3 * voice_output + 0.3 * text_output + 0.4 * sight_output
-
-# Action 10: Implement Hybrid Fusion
-def hybrid_fusion(early_fusion_output, late_fusion_output):
-    return 0.5 * early_fusion_output + 0.5 * late_fusion_output
-
-# Action 11: Design Model Architecture
-
-
-
-
-
-def transform_voice(normalized_audio):
-    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-    input_values = processor(normalized_audio, return_tensors="pt").input_values
-    with torch.no_grad():
-        logits = model(input_values).logits
-    return logits
-
-def transform_text(tokenized_text):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertModel.from_pretrained('bert-base-uncased')
-    inputs = tokenizer(tokenized_text, return_tensors="pt")
-    outputs = model(**inputs)
-    return outputs.last_hidden_state
-
-def transform_sight(loaded_image):
-    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
-    model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-    inputs = feature_extractor(loaded_image, return_tensors="pt")
-  
-def design_model(early_fusion_output, late_fusion_output):
-    voice_input = tf.keras.layers.Input(shape=(early_fusion_output.shape[1],))  # Adjusted
-    text_input = tf.keras.layers.Input(shape=(early_fusion_output.shape[1],))   # Adjusted
-    sight_input = tf.keras.layers.Input(shape=(early_fusion_output.shape[1],))  # Adjusted
-    voice_logits = transform_voice(normalized_audio)
-    text_vector = transform_text(tokenized_text)
-    sight_vector = transform_sight(loaded_image)
-    early_fusion_output = early_fusion(voice_logits, text_vector, sight_vector)
-    late_fusion_output = late_fusion(voice_logits, text_vector, sight_vector)
-    hybrid_fusion_output = hybrid_fusion(early_fusion_output, late_fusion_output)
-    model = build_multimodal_model(early_fusion_output.shape[1])
-    model.compile(optimizer='adam', loss=custom_loss)
-
-# Assuming y_true_voice, y_true_text, and y_true_sight are the true labels for each modality
-def custom_loss(y_true_voice, y_true_text, y_true_sight, y_pred_voice, y_pred_text, y_pred_sight):
-    voice_loss = tf.keras.losses.MeanSquaredError()(y_true_voice, y_pred_voice)
-    text_loss = tf.keras.losses.MeanSquaredError()(y_true_text, y_pred_text)
-    sight_loss = tf.keras.losses.MeanSquaredError()(y_true_sight, y_pred_sight)
-    fusion_loss = voice_loss + text_loss + sight_loss
-    final_loss = voice_loss + text_loss + sight_loss + 0.1 * fusion_loss
-    return final_loss
-                      
-
-# Model Architecture
 def build_multimodal_model(input_shape):
     input_layer = tf.keras.layers.Input(shape=input_shape)
     x = tf.keras.layers.Dense(128, activation='relu')(input_layer)
     x = tf.keras.layers.Dense(64, activation='relu')(x)
-    output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+    output_layer = tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')(x)
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
     return model
 
 # Learning Rate Schedule
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_lr, decay_steps=decay_steps, decay_rate=decay_rate, staircase=True)
+initial_lr = 0.001
+decay_steps = 10000
+decay_rate = 0.9
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_lr, decay_steps, decay_rate, staircase=True)
 
 # Optimizer
-optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
-# Base ResNet-152 Model
-base_model = tf.keras.applications.ResNet152(weights='imagenet', include_top=False, input_shape=sight_input_shape)
-
-# Custom Convolutional Layers
-conv_custom1 = tf.keras.layers.Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu')
-conv_custom2 = tf.keras.layers.Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu')
-
-# Model Architecture
-voice_input = tf.keras.layers.Input(shape=voice_input_shape)
-text_input = tf.keras.layers.Input(shape=text_input_shape)
-sight_input = tf.keras.layers.Input(shape=sight_input_shape)
-
-# Voice, Text, and Sight Features
-voice_features = tf.keras.layers.LSTM(128)(voice_input)
-text_features = tf.keras.layers.Dense(128, activation='relu')(text_input)
-sight_features = base_model(sight_input)
-sight_features = conv_custom1(sight_features)
-sight_features = conv_custom2(sight_features)
-sight_features = tf.keras.layers.GlobalAveragePooling2D()(sight_features)
-
-# Fusion Mechanism
-fused_features = tf.keras.layers.Concatenate()([voice_features, text_features, sight_features])
-fused_features = tf.keras.layers.Dropout(0.5)(fused_features)
-
-# Fully Connected Layer
-output = tf.keras.layers.Dense(num_classes, activation='softmax')(fused_features)
-
-# Final Model
-model = tf.keras.Model(inputs=[voice_input, text_input, sight_input], outputs=output)
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
 # Compile Model
-model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+model = build_multimodal_model(early_fusion_output.shape[1])
+model.compile(optimizer=optimizer, loss=custom_loss, metrics=['accuracy'])
 
-# Summary
+# Model Summary
 model.summary()
